68c1b8ebac0f085c23f831fb31c1876d850057f7
Paul Molnar <saintpaul55012@yahoo.com>
2019-05-17 16:03:30 -0500

    Append to table functionality working.

--- client.js
+++ client.js
@@ -1,1 +1,29 @@
-console.log("Testing");
+$(document).ready(onReady);
+
+function onReady() {
+    $("#salaryButton").on('click', calculateMonthly);
+
+}
+
+function calculateMonthly() {
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let id = $('#idIn').val();
+    let title = $('#titleIn').val();
+    let annual = $('#annualIn').val();
+    // $("tbody").append('<tr><td>'+firstName+'</td><td</tr>');
+    $("tbody").append(`   
+    <tr>
+        <td>`+firstName+`</td>
+        <td>` +lastName+ `</td>
+        <td>` +id+ `</td>
+        <td>` +title+ `</td>
+        <td>` +annual+ `</td>
+        <td><button>Delete</button></td>
+    
+    </tr>`)
+
+    $('.inputs').val(" ");
+
+
+}

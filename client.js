7b489a7424195b9f1db3aedf01a8a016ec481f9a
Paul Molnar <saintpaul55012@yahoo.com>
2019-05-18 16:20:54 -0500

    Base goals finished.

--- client.js
+++ client.js
@@ -1,31 +1,50 @@
 $(document).ready(onReady);
 
+let totalMonthly = 10000;
+let annual;
 function onReady() {
     $("#salaryButton").on('click', calculateMonthly);
+    $("#tableDiv").on('click', '.deleteButton', deleteEmployee);
 
 }
-
+// Store input values as variables
 function calculateMonthly() {
     let firstName = $('#firstNameIn').val();
     let lastName = $('#lastNameIn').val();
     let id = $('#idIn').val();
     let title = $('#titleIn').val();
-    let annual = $('#annualIn').val();
-    // $("tbody").append('<tr><td>'+firstName+'</td><td</tr>');
+    annual = $('#annualIn').val();
+
+    // Show input values in table
     $("tbody").append(`   
     <tr>
         <td>`+ firstName + `</td>
         <td>` + lastName + `</td>
         <td>` + id + `</td>
         <td>` + title + `</td>
-        <td>` + annual + `</td>
-        <td><button>Delete</button></td>
-    
+        <td>` + Number(annual) + `</td>
+        <td><button class="deleteButton">Delete</button></td>
     </tr>`);
 
-    $('.inputs').val(" ");
+    // Clear input values
+    $('.inputs').val("");
 
-    $("#monthly").append(Math.floor(annual/12));
+    // Calulate and format total 
+    let formattedMonthly = totalMonthly += Number(annual) / 12;
+    let formattedFormat = formattedMonthly.toFixed(2)
+    $('#monthly').text(formattedFormat);
 
+    // Make red if over 20000
+    if (formattedFormat > 20000) {
+        $('#monthly').css('color', 'red');
+    }
+}
+
+function deleteEmployee() {
+    $(this).closest('tr').remove();
 
 }
+
+
+
+

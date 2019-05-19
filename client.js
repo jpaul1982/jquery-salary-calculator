350dc1d6324196cc75b1f841c552f17121389c17
Paul Molnar <saintpaul55012@yahoo.com>
2019-05-19 17:26:53 -0500

    jQuery ssalary calculator base goals with styling finished.

--- client.js
+++ client.js
@@ -2,11 +2,21 @@ $(document).ready(onReady);
 
 let totalMonthly = 10000;
 let annual;
+let employeeList = [
+    {
+        firstName: "Captain",
+        lastName: "Tripps",
+        id: Number(19),
+        title: 'Traveler',
+        annual: 120000
+    }
+];
+
 function onReady() {
     $("#salaryButton").on('click', calculateMonthly);
     $("#tableDiv").on('click', '.deleteButton', deleteEmployee);
-
 }
+
 // Store input values as variables
 function calculateMonthly() {
     let firstName = $('#firstNameIn').val();
@@ -15,6 +25,11 @@ function calculateMonthly() {
     let title = $('#titleIn').val();
     annual = $('#annualIn').val();
 
+    let employee = { firstName, lastName, id, title, annual };
+    console.log(employee);
+    employeeList.push(employee);
+    console.log(employeeList);
+
     // Show input values in table
     $("tbody").append(`   
     <tr>
@@ -22,8 +37,8 @@ function calculateMonthly() {
         <td>` + lastName + `</td>
         <td>` + id + `</td>
         <td>` + title + `</td>
-        <td>` + Number(annual) + `</td>
-        <td><button class="deleteButton">Delete</button></td>
+        <td>$` + Number(annual) + `</td>
+        <td><button id=${Number(id)} class="deleteButton">Delete</button></td>
     </tr>`);
 
     // Clear input values
@@ -31,19 +46,28 @@ function calculateMonthly() {
 
     // Calulate and format total 
     let formattedMonthly = totalMonthly += Number(annual) / 12;
-    let formattedFormat = formattedMonthly.toFixed(2)
-    $('#monthly').text(formattedFormat);
+    let newFormattedMonthly = formattedMonthly.toFixed(2)
+    $('#monthly').text(newFormattedMonthly);
 
     // Make red if over 20000
-    if (formattedFormat > 20000) {
-        $('#monthly').css('color', 'red');
+    if (newFormattedMonthly > 20000) {
+        $('#monthly').css('color', '#990404');
     }
 }
 
+// Remove employee from list
 function deleteEmployee() {
+    // Remove closest table row
     $(this).closest('tr').remove();
-
 }
+
+
+
+
+
+
+
+
 
 
 
